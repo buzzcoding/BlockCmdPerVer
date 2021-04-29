3732bd7ad9c5d5fb1a84ce72ce019f6576909b22
BuzzTheGamer23 <buzzthegamer23@gmail.com>
2021-04-28 21:24:28 -0500

    Add bypass perm and refactoring changes

--- src/main/java/com/buzzthegamer23/blockcmdperver/CommandDisabler.java
+++ src/main/java/com/buzzthegamer23/blockcmdperver/CommandDisabler.java
@@ -6,15 +6,16 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CommandDisabler implements Listener {
 	@EventHandler
-	public void onTeleport(PlayerCommandPreprocessEvent e) {
+	public void onCommand(PlayerCommandPreprocessEvent e) {
+		if (e.getPlayer().hasPermission("bcpver.bypass")) return;
 		try {
 			if (!Config.getBoolean("enabled")) return;
-			if (Config.getList(Main.getVersionName(Main.getPlayerProto(e.getPlayer()))).contains(e.getMessage().replaceFirst("/", "").split(" ")[0])) {
+			if (Config.getList(BlockCmdPerVer.getVersionName(BlockCmdPerVer.getPlayerProto(e.getPlayer()))).contains(e.getMessage().replaceFirst("/", "").split(" ")[0])) {
 				e.getPlayer().sendMessage("That command is disabled on this version. Please join on another version.");
 				e.setCancelled(true);
 			}
 		} catch (Exception e2) {
-			Main.warn(Main.prefix + "[" + Main.pluginname + "] Failed to detect player-sent command. The full error is below.");
+			BlockCmdPerVer.warn("Failed to detect player-sent command. The full error is below.");
 			e2.printStackTrace();
 		}
 	}
